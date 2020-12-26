ff94dc63d351d030210c256eb718b6ea852cc714
zhang <873853298@qq.com>
2020-12-26 23:21:16 +0800

    zhang

---
+++ app/Model/UserPostAccess.php
@@ -0,0 +1,15 @@
+<?php
+
+declare (strict_types=1);
+
+namespace App\Model;
+
+use Hyperf\DbConnection\Model\Model;
+use Hyperf\HttpServer\Contract\RequestInterface;
+
+/**
+ */
+class UserPostAccess extends Model
+{
+
+}
